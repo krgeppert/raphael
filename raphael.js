ea806368ad44f4c5d5e5a84ae490d3719bf47141
Kyle Geppert <krgeppert@gmail.com>
2015-07-17 14:38:23 -0700

    Update raphael.js
    WOoo

--- raphael.js
+++ raphael.js
@@ -375,7 +375,7 @@
     eve.toString = function () {
         return "You are running Eve " + version;
     };
-    (typeof module != "undefined" && module.exports) ? (module.exports = eve) : (typeof define != "undefined" ? (define("eve", [], function() { return eve; })) : (glob.eve = eve));
+    glob.eve = eve;
 })(this);
 // ┌─────────────────────────────────────────────────────────────────────┐ \\
 // │ "Raphaël 2.1.2" - JavaScript Vector Library                         │ \\
